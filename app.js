01866674839f3146424769be4a8cc325860167c4
Liu <hnnk@qq.com>
2020-12-14 16:05:10 +0800

    object.aaa works

--- app.js
+++ app.js
@@ -42,9 +42,41 @@ router.get('/hand2', (ctx) => {
   var data = {};
   names.forEach( (v,i) => data[v] = i + ' is ' + Math.random());
 
+  // ctx.body = Handlebars.compile(parsedTemplate)(data);
+  ctx.body = parsedTemplate;
+
+})
+
+// Handlebars AST parse test.
+// object.aaa test  // TODO for each test
+router.get('/hand3', (ctx) => {
+  var str = `Name: {{page.name}}
+    Bye: {{page.bye}}
+  `;
+  const parsedTemplate = Handlebars.parse(str);
+  // ctx.body = parsedTemplate;
+  var body = parsedTemplate.body.filter((v)=> {
+    // fileter Mustache expression object from array
+    if (v.type === 'MustacheStatement')
+    return true;
+  })
+  var parts = body.map(v => (v.path && v.path.parts) || '');
+
+  let data = {}
+  parts.forEach((outerV, outerI) => {
+    console.log(outerV,outerV[0],outerV[1]);
+    
+    if (!data[outerV[0]]) {
+      data[outerV[0]] = {}
+    }
+    data[outerV[0]][outerV[1]] = Math.random();
+  });
+  console.log(data)
   ctx.body = Handlebars.compile(parsedTemplate)(data);
 
 })
+
+
 app
   .use(router.routes())
   .use(router.allowedMethods());
